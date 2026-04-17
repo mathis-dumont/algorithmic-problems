aa24d426dd2aed631249bb4f187880d78a883563
mathis-dumont <64702984+mathis-dumont@users.noreply.github.com>
2026-04-17 17:02:13 +0200

    Add: search-for-word-ii - submission-5

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-5.py
@@ -0,0 +1,49 @@
+class NodeTrie:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+    
+    def addWord(self, word):
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = NodeTrie()
+            cur = cur.children[c]
+        cur.is_end = True
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        rows, cols = len(board), len(board[0])
+        root = NodeTrie()
+
+        for word in words:
+            root.addWord(word)
+
+        res = set()
+
+        def dfs(r,c,node,word):
+            if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] == '#' or board[r][c] not in node.children:
+                return 
+
+            else:
+                # if board[r][c] in node.children and not visited
+                word += board[r][c]
+                node = node.children[board[r][c]]
+                if node.is_end:
+                    res.add(word)
+
+                tmp = board[r][c]
+                board[r][c] = '#'
+                
+                dfs(r-1,c,node,word)
+                dfs(r+1,c,node,word)
+                dfs(r,c-1,node,word)
+                dfs(r,c+1,node,word)
+
+                board[r][c] = tmp
+
+        for i in range(rows):
+            for j in range(cols):
+                dfs(i,j,root,"")
+
+        return list(res)
