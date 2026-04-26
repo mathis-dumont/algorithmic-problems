676fb4c828d608a8b56bd3fdfc07dbb9e0ade7fe
mathis-dumont <64702984+mathis-dumont@users.noreply.github.com>
2026-04-26 18:05:39 +0200

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        courses = prerequisites
+        g = {i:[] for i in range(numCourses)}
+        UNVISITED, VISITING, VISITED = 0, 1, 2
+        states = [UNVISITED] * numCourses
+
+        for a,b in courses:
+            g[a].append(b)
+        
+        def dfs(node):
+            if states[node] == VISITING:
+                return False
+            if states[node] == VISITED:
+                return True
+            states[node] = VISITING
+
+            for neighbor in g[node]:
+                if not dfs(neighbor): return False
+                
+            states[node] = VISITED
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True
