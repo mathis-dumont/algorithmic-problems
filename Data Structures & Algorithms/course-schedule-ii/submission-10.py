9a47f9d28e65e53e9b21abd2254bc6e476d9f6c3
mathis-dumont <64702984+mathis-dumont@users.noreply.github.com>
2026-05-11 23:55:29 +0200

    Add: course-schedule-ii - submission-10

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-10.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        g = {i:[] for i in range(numCourses)}
+        in_degree = [0]*numCourses
+        order = []
+
+        for a,b in prerequisites:
+            g[b].append(a)
+            in_degree[a] += 1
+
+        q = collections.deque([i for i in range(numCourses) if in_degree[i] == 0])
+
+        while q:
+            n = q.popleft()
+            order.append(n)
+            for neighbor in g[n]:
+                in_degree[neighbor] -=1
+                if in_degree[neighbor] == 0:
+                    q.append(neighbor)
+        if len(order) == numCourses:
+            return order
+        else:
+            return []
+
+
