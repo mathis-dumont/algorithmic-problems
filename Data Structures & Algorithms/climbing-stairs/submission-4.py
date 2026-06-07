eaeab6e97d24bc8d6109747632c1ecb4a087a410
mathis-dumont <64702984+mathis-dumont@users.noreply.github.com>
2026-06-07 16:44:57 +0200

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+
+        def dp(i):
+            if i <= 1:
+                return 1
+            if i not in memo:
+                memo[i] = dp(i-1) + dp(i-2)
+            
+            return memo[i]
+
+        return dp(n)
