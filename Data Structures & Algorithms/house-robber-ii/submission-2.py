795ea94db2f99599b05e84be31e127dffa11e39a
mathis-dumont <64702984+mathis-dumont@users.noreply.github.com>
2026-06-09 22:57:22 +0200

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        if n==1:
+            return nums[0]
+        if n==2:
+            return max(nums[0],nums[1])
+        
+        def dp(i,l,memo):
+            if i==1:
+                return l[0]
+            if i==2:
+                return max(l[0],l[1])
+            if i not in memo:
+                memo[i] = max(dp(i-2,l,memo)+l[i-1], dp(i-1,l,memo))
+            return memo[i]
+        
+        return max(dp(n-1,nums[1:],{}),dp(n-1,nums[:n],{}))
+
+        
+
