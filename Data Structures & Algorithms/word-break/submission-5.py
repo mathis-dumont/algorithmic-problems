2305e99bdb5a3e836531bd9d4bb9486c8d7c5036
mathis-dumont <64702984+mathis-dumont@users.noreply.github.com>
2026-06-17 22:11:08 +0200

    Add: word-break - submission-5

---
+++ Data Structures & Algorithms/word-break/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = {}
+        wordDict = set(wordDict)
+        def dp(i):
+            if i == 0:
+                return True
+            if i not in memo:
+                memo[i] = False
+                for j in range(1,i+1): 
+                    if s[i-j:i] in wordDict and dp(i-j):
+                        memo[i] = True
+                        break
+            return memo[i]
+        return dp(len(s))
+            
